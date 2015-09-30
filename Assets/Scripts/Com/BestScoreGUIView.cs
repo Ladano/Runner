0010c85caf10f4bc2f05f543b782f5f49fa4c36c
Ladano <aspidmorok@rambler.ru>
2015-09-30 17:01:24 +0700

    best score
    добавление рекорда и его отображения в меню

---
+++ Assets/Scripts/Com/BestScoreGUIView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Com
+{
+	public class BestScoreGUIView : MonoBehaviour
+	{
+		private const string BestScoreFormatString = "Best score: {0}";
+
+		[SerializeField] private Text _label;
+
+		private void Start()
+		{
+			_label.text = string.Format(BestScoreFormatString, GlobalValues.BestScore.ToString());
+		}
+	}
+}
---
+++ Assets/Scripts/Com/GlobalValues.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Com
+{
+	public static class GlobalValues
+	{
+		private const string BestScorePrefsName = "PLAYER_BEST_SCORE";
+
+		public static int BestScore
+		{
+			get { return PlayerPrefs.GetInt(BestScorePrefsName, 0); }
+			set { PlayerPrefs.SetInt(BestScorePrefsName, value); }
+		}
+	}
+}

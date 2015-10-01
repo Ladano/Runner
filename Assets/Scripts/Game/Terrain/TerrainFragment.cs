80ca3a154f68d2e03c79d19711a11e7dc670ecf2
Ladano <ladanoas@gmail.com>
2015-10-01 22:05:07 +0600

    terrain generation
    terrain generator - закончен
    terrain fragment - пока пустой скрипт

---
+++ Assets/Scripts/Game/Terrain/TerrainFragment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Com.Game
+{
+	public class TerrainFragment : MonoBehaviour
+	{
+		[SerializeField] private Transform _nextTerrainWp;
+
+		public void Init()
+		{
+
+		}
+
+		public Transform GetNextTerrainWp()
+		{
+			return _nextTerrainWp;
+		}
+
+		public void ReleaseAllObjects()
+		{
+
+		}
+	}
+}
--- Assets/Scripts/Game/TerrainGenerator.cs
+++ Assets/Scripts/Game/TerrainGenerator.cs
@@ -3,8 +3,61 @@ using System.Collections;
 
 namespace Com.Game
 {
-	public class TerrainGenerator : MonoBehaviour
+	public class TerrainGenerator : AbstractGameController
 	{
+		[SerializeField] private GameObject _terrainPrefab;
+		[SerializeField] private int _startTerrainCount = 3;
+		[SerializeField] private Transform _startTerrainPos;
+		[SerializeField] private Transform _rootTerrain;
+		[SerializeField] private float _speed = 1.0f;
+		private GenericPool<TerrainFragment> _terrainPool;
 
+		protected override void Init()
+		{
+			_terrainPool = new GenericPool<TerrainFragment>(_terrainPrefab, (uint)_startTerrainCount);
+			for(int i=1; i<=_startTerrainCount; i++)
+			{
+				GenerateTerrain();
+			}
+		}
+
+		protected override void StartGame()
+		{
+
+		}
+
+		protected override void EndGame()
+		{
+
+		}
+
+		private void GenerateTerrain()
+		{
+			TerrainFragment terrain = _terrainPool.GetObjectFromPool();
+			terrain.transform.parent = _rootTerrain;
+			terrain.transform.position = _startTerrainPos.position;
+			_startTerrainPos = terrain.GetNextTerrainWp();
+		}
+
+		private void ReleaseTerrain(TerrainFragment terrain)
+		{
+			_terrainPool.ReleaseObject(terrain);
+		}
+
+		public void SpawnNewTerrain()
+		{
+			TerrainFragment terrain = _terrainPool.GetFirstActiveObject();
+			terrain.ReleaseAllObjects();
+			ReleaseTerrain(terrain);
+			GenerateTerrain();
+		}
+
+		private void Update()
+		{
+			if(_isGameActive)
+			{
+				_rootTerrain.Translate(Vector3.back * Time.deltaTime * _speed);
+			}
+		}
 	}
 }

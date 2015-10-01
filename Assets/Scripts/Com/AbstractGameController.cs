083108b6376ff13d4abc22ff7e5f938376b35777
Ladano <ladanoas@gmail.com>
2015-10-01 21:57:12 +0600

    added abstract game controller

---
+++ Assets/Scripts/Com/AbstractGameController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Com.Game
+{
+	public abstract class AbstractGameController : MonoBehaviour
+	{
+		protected bool _isGameActive = false;
+
+		private void OnEnable()
+		{
+			GameController.OnGameStart += StartGameEvent;
+			GameController.OnGameOver += EndGameEvent;
+		}
+
+		private void OnDisable()
+		{
+			GameController.OnGameStart -= StartGameEvent;
+			GameController.OnGameOver -= EndGameEvent;
+		}
+
+		private void Awake()
+		{
+			Init();
+		}
+
+		private void StartGameEvent()
+		{
+			_isGameActive = true;
+			StartGame();
+		}
+
+		private void EndGameEvent()
+		{
+			_isGameActive = false;
+			EndGame();
+		}
+
+		protected abstract void Init();
+
+		protected abstract void StartGame();
+
+		protected abstract void EndGame();
+	}
+}
